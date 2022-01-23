ba2dc796afab46cfb2730f2d14a552f039379a3c
Fabio Mauersberger <thebreadcompany@icloud.com>
2022-01-23 19:25:00 +0100

    remove test code because not yet existing

--- Tests/cursesKitTests/cursesKitTests.swift
+++ Tests/cursesKitTests/cursesKitTests.swift
@@ -1,11 +1,2 @@
 import XCTest
 @testable import cursesKit
-
-final class cursesKitTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(cursesKit().text, "Hello, World!")
-    }
-}
