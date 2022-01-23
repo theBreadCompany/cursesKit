34cec038b9d1a7792bd0df77837234b13aa1febc
Fabio Mauersberger <thebreadcompany@icloud.com>
2022-01-23 19:25:39 +0100

    add ncurses with bridging header

---
+++ Sources/ncurses/ncurses.h
@@ -0,0 +1,17 @@
+//
+//  ncurses.h
+//  
+//
+//  Created by Fabio Mauersberger on 21.01.22.
+//
+
+#ifndef ncurses_helper_h
+#define ncurses_helper_h
+
+#define _XOPEN_SOURCE 500
+#include <curses.h>
+#include <stdlib.h>
+
+#define TEXT_BOLD   A_BOLD
+
+#endif /* ncurses_helper_h */
